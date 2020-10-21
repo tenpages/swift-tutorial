ccc56c26db4aacd01ce6512af513e9d0fb4006b2
Shuo Liu <tenpages66@163.com>
2020-10-20 22:50:44 -0400

    Project 1: WeSplit

---
+++ README.md
@@ -0,0 +1,18 @@
+# swift-tutorial
+I plan on learning swift following the tutorial provided [here](https://www.hackingwithswift.com/100/swiftui). I will put the codes I created during the process here.
+
+Since I'm not full-time on this, I might not be able to follow exactly after the Days requirement in that tutorial. But still, I hope I will be learning things in a fair period of time.
+
+## Contents
+
+### 01 - WeSplit
+
+![WeSplit Image](images/proj1-wesplit.png)
+
+Things learned:
+
+* Intro to SwiftUI
+* Text, TextField, Form, Section, NavigationView, Picker
+* .navigationBarTitle(), pickerStyle()
+* @State, $ (two-way operation)
+
---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by tenpages on 10/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    //@State private var numberOfPeopleStr = ""
+    @State private var numberOfPeople = 1
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    /*
+    var numberOfPeople: Int {
+        return Int(numberOfPeopleStr) ?? 1
+    }
+    */
+    var grandTotal: Double {
+        let tipSelection = Double(self.tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount * tipSelection / 100
+        let grandTotal = orderAmount + tipValue
+        return grandTotal
+    }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach (2..<100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(DefaultPickerStyle())
+                    /*
+                    TextField("Number of people", text: $numberOfPeopleStr)
+                        .keyboardType(.numberPad)
+                    */
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach (0..<self.tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Grand total")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
+}
